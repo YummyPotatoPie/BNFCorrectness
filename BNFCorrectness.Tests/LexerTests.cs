750ae4a11286cafac8853b8f187243affb51fe2f
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-07-20 03:18:43 +0300

    Updated tests

---
+++ BNFCorrectness.Tests/LexerTests.cs
@@ -0,0 +1,75 @@
+using System;
+using SymbolStreams;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BNFCorrectness.Tests
+{
+    public class LexerTests
+    {
+        [Fact]
+        public void CorrectTokenBuildTest()
+        {
+            // Arrange 
+            string testCase = "<rule> ::= <another-rule> | \"\"\n";
+            Lexer lexer = new(new LoadedStream<char>(testCase.ToCharArray()));
+            Token[] expectedTokens =
+            {
+                new Token('<'),
+                new WordToken("rule", (int)TokenTag.Literal),
+                new Token('>'),
+                WordToken.ProductionOperator,
+                new Token('<'),
+                new WordToken("another-rule", (int)TokenTag.Literal),
+                new Token('>'),
+                new Token('|'),
+                new Token('"'),
+                new Token('"'),
+                new Token('\n')
+            };
+
+            // Act
+            List<Token> actualTokens = new();
+            Token currentToken = lexer.GetNextToken();
+
+            while (currentToken != null)
+            {
+                actualTokens.Add(currentToken);
+                currentToken = lexer.GetNextToken();
+            }
+
+            // Assert
+            Assert.True(IsArraysEqual(expectedTokens, actualTokens.ToArray()));
+        }
+
+        private static bool IsArraysEqual<T>(T[] arrayA, T[] arrayB)
+        {
+            if (arrayA.Length != arrayB.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < arrayA.Length; i++)
+                    if (arrayA[i].ToString() != arrayB[i].ToString())
+                        return false;
+                return true;
+            }
+        }
+
+        [Fact]
+        public void SyntaxErrorThrownTest()
+        {
+            Action actionCode = SyntaxErrorThrownActionCode;
+            Assert.Throws<SyntaxError>(actionCode);
+        }
+
+        private void SyntaxErrorThrownActionCode()
+        {
+            string testCase = "< := :>";
+            Lexer lexer = new(new LoadedStream<char>(testCase.ToCharArray()));
+
+            while (lexer.GetNextToken() != null) lexer.GetNextToken();
+        }
+    }
+}
