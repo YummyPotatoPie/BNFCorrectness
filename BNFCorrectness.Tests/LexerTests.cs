ff57fc1fccd7cf6040c0b75a85db2b00a3171e69
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-07-20 19:30:04 +0300

    Updated some tests and added new test

--- BNFCorrectness.Tests/LexerTests.cs
+++ BNFCorrectness.Tests/LexerTests.cs
@@ -16,15 +16,14 @@ namespace BNFCorrectness.Tests
             Token[] expectedTokens =
             {
                 new Token('<'),
-                new WordToken("rule", (int)TokenTag.Literal),
+                new WordToken("rule", (int)TokenTag.RuleID),
                 new Token('>'),
                 WordToken.ProductionOperator,
                 new Token('<'),
-                new WordToken("another-rule", (int)TokenTag.Literal),
+                new WordToken("another-rule", (int)TokenTag.RuleID),
                 new Token('>'),
                 new Token('|'),
-                new Token('"'),
-                new Token('"'),
+                new WordToken("", (int)TokenTag.Literal),
                 new Token('\n')
             };
 
@@ -70,6 +69,37 @@ namespace BNFCorrectness.Tests
             Lexer lexer = new(new LoadedStream<char>(testCase.ToCharArray()));
 
             while (lexer.GetNextToken() != null) lexer.GetNextToken();
+        }
+
+        [Fact]
+        public void LiteralReadTest()
+        {
+            // Arrange
+            string testCase = "<rule> ::= \"Hello\" | 'World'";
+            Lexer lexer = new(new LoadedStream<char>(testCase.ToCharArray()));
+            Token[] expectedTokens =
+            {
+                new Token('<'),
+                new WordToken("rule", (int)TokenTag.RuleID),
+                new Token('>'),
+                WordToken.ProductionOperator,
+                new WordToken("Hello", (int)TokenTag.Literal),
+                new Token('|'),
+                new WordToken("World", (int)TokenTag.Literal),
+            };
+
+            // Act
+            List<Token> actualTokens = new();
+            Token currentToken = lexer.GetNextToken();
+
+            while (currentToken != null)
+            {
+                actualTokens.Add(currentToken);
+                currentToken = lexer.GetNextToken();
+            }
+
+            // Assert
+            Assert.True(IsArraysEqual(expectedTokens, actualTokens.ToArray()));
         }
     }
 }
