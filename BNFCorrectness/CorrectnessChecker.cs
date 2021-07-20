519d853d1daa7f3daa267d4aa8741cba801e8e7a
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-07-21 02:06:27 +0300

    Program main file

--- BNFCorrectness/CorrectnessChecker.cs
+++ BNFCorrectness/CorrectnessChecker.cs
@@ -1,13 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using SymbolStreams;
 
 namespace BNFCorrectness
 {
     class CorrectnessChecker
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string grammarFile = args[0];
 
+            if (Path.GetExtension(grammarFile) != ".bnf")
+            {
+                Console.WriteLine("Invalid file extension or file does not exist");
+                return;
+            }
+
+            try
+            {
+                StreamReader reader = new(grammarFile);
+                Lexer lexer = new(new LoadedStream<char>(reader.ReadToEnd().ToCharArray()));
+
+                List<Token> parsedTokens = new();
+                Token currentToken = lexer.GetNextToken();
+
+                while (currentToken != null)
+                {
+                    parsedTokens.Add(currentToken);
+                    currentToken = lexer.GetNextToken();
+                }
+
+                SyntaxAnalyser syntaxAnalyser = new(lexer.Table, parsedTokens.ToArray());
+                syntaxAnalyser.Parse();
+            }
+            catch (SyntaxError ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
