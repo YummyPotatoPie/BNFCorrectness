631172f25788b8bf0045da581486dd0ebdfef5d1
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-07-20 22:35:04 +0300

    Added tests for SyntaxAnalyser

---
+++ BNFCorrectness.Tests/SyntaxAnalyserTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using SymbolStreams;
+
+namespace BNFCorrectness.Tests
+{
+    public class SyntaxAnalyserTests
+    {
+        [Fact]
+        public void CorrectSyntaxTest()
+        {
+            string testCase = "<rule> ::= <hello-world> | 'aboba'\n <hello-world> ::= 'Hello' | 'World'";
+            List<Token> parsedTokens = new();
+            Lexer lexer = new(new LoadedStream<char>(testCase.ToCharArray()));
+
+            Token currentToken = lexer.GetNextToken();
+
+            while (currentToken != null)
+            {
+                parsedTokens.Add(currentToken);
+                currentToken = lexer.GetNextToken();
+            }
+
+            SyntaxAnalyser syntaxAnalyser = new(lexer.Table, parsedTokens.ToArray());
+            syntaxAnalyser.Parse();
+        }
+    }
+}
