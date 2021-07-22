e877d2deeede01b7ffcc59269915ad77a31b2a74
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-07-22 04:56:10 +0300

    Added test for syntax analyser

--- BNFCorrectness.Tests/SyntaxAnalyserTests.cs
+++ BNFCorrectness.Tests/SyntaxAnalyserTests.cs
@@ -24,5 +24,24 @@ namespace BNFCorrectness.Tests
             SyntaxAnalyser syntaxAnalyser = new(lexer.Table, parsedTokens.ToArray());
             syntaxAnalyser.Parse();
         }
+
+        [Fact]
+        public void RegexSyntaxTest()
+        {
+            string testCase = "<rule> ::= ([a-z]|[A-Z]) | (abs[a-z]?)";
+            List<Token> parsedTokens = new();
+            Lexer lexer = new(new LoadedStream<char>(testCase.ToCharArray()));
+
+            Token currentToken = lexer.GetNextToken();
+
+            while (currentToken != null)
+            {
+                parsedTokens.Add(currentToken);
+                currentToken = lexer.GetNextToken();
+            }
+
+            SyntaxAnalyser syntaxAnalyser = new(lexer.Table, parsedTokens.ToArray());
+            syntaxAnalyser.Parse();
+        }
     }
 }
